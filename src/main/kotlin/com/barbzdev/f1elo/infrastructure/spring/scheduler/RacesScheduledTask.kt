c6abd4a5b807f419a4f4548f5fcd3c48dc917e65
barbz <eduardob907@gmail.com>
2024-11-02 00:45:06 +0100

    feat(job): add flags for schedule job

--- src/main/kotlin/com/barbzdev/f1elo/infrastructure/spring/scheduler/RacesScheduledTask.kt
+++ src/main/kotlin/com/barbzdev/f1elo/infrastructure/spring/scheduler/RacesScheduledTask.kt
@@ -1,11 +1,19 @@
 package com.barbzdev.f1elo.infrastructure.spring.scheduler
 
 import com.barbzdev.f1elo.application.GatherRacesBySeasonUseCase
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
-class RacesScheduledTask(private val gatherRacesBySeasonUseCase: GatherRacesBySeasonUseCase) {
+class RacesScheduledTask(
+  private val gatherRacesBySeasonUseCase: GatherRacesBySeasonUseCase, @Value("\${cron.job.enabled}") private val isCronJobEnabled: Boolean
+) {
 
-  @Scheduled(cron = "0 0 12 * * ?") fun gatherRacesResultBySeasonScheduledTask() = gatherRacesBySeasonUseCase.invoke()
+  @Scheduled(cron = "0 0 12 * * ?")
+  fun gatherRacesResultBySeasonScheduledTask() {
+    if (isCronJobEnabled) {
+      gatherRacesBySeasonUseCase.invoke()
+    }
+  }
 }
