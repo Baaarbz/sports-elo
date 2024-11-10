032ced814eddb391b18cb561b41b22ce39eb32a6
Eduardo Barbosa <eduardob907@gmail.com>
2024-11-10 15:54:52 +0000

    feat(prometheus): add actuator, metrics and prometheus (#6)
    * feat(metrics): add metrics to improve observability
    
    * feat(prometheus): add micrometer registry prometheus dependency, this dependency formats actuator metrics into a Prometheus-readable format
    
    * feat(prometheus): expose actuator prometheus endpoints
    
    * feat(prometheus): add script to build up the infrastructure with prometheus amd all config and docker compose required
    
    * chore: run ktfmt
    
    * fix: stop using root user in prometheus Dockerfile

--- src/main/kotlin/com/barbzdev/f1elo/infrastructure/spring/configuration/ObservabilityConfiguration.kt
+++ src/main/kotlin/com/barbzdev/f1elo/infrastructure/spring/configuration/ObservabilityConfiguration.kt
@@ -1,11 +1,19 @@
 package com.barbzdev.f1elo.infrastructure.spring.configuration
 
+import com.barbzdev.f1elo.domain.observability.MetricClient
 import com.barbzdev.f1elo.infrastructure.observability.DefaultUseCaseInstrumentation
+import com.barbzdev.f1elo.infrastructure.observability.MicrometerMetricClient
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class ObservabilityConfiguration {
 
-  @Bean fun useCaseInstrumentation() = DefaultUseCaseInstrumentation()
+  @Bean fun useCaseInstrumentation(metricClient: MetricClient) = DefaultUseCaseInstrumentation(metricClient)
+
+  @Bean fun threadMetrics() = JvmThreadMetrics()
+
+  @Bean fun metricClient(meterRegistry: MeterRegistry) = MicrometerMetricClient(meterRegistry)
 }
