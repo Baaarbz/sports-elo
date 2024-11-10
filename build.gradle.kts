ba4e2f4de1b48ace93adec38a7afe94a8110dc57
baaarbz <eduardob907@gmail.com>
2024-11-10 11:35:27 +0100

    feat(prometheus): add micrometer registry prometheus dependency, this dependency formats actuator metrics into a Prometheus-readable format

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
 
+  implementation("io.micrometer:micrometer-registry-prometheus:1.13.6")
 
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
