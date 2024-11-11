2d6e0ad9d1bb410c168a3f2d2195cbce176d4b1d
barbz <eduardob907@gmail.com>
2024-11-11 23:04:43 +0100

    feat(infrastructure): wait to deploy infrastructure

--- deploy/deploy_infrastructure.sh
+++ deploy/deploy_infrastructure.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 docker compose -f infrastructure/docker-compose.yml up -d --build
+
+while ! docker compose -f infrastructure/docker-compose.yml ps | grep -q "Up"; do
+  echo "Waiting for services to be up..."
+  sleep 1
+done
