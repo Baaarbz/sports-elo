db6bf973acdde32d40b74319ddd2bd58d942b2e2
baaarbz <eduardob907@gmail.com>
2024-11-18 23:14:46 +0100

    feat(cd/ci): create deploy script and use it for deploy as workaround not working env variables

---
+++ deploy/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Check if Prometheus and Postgres services are up
+if ! docker ps | grep -q prometheus || ! docker ps | grep -q postgres; then
+  chmod +x ./f1-elo/deploy/deploy_infrastructure.sh
+  ./f1-elo/deploy/deploy_infrastructure.sh
+fi
+
+FILE="f1-elo/src/main/resources/application-pro.yml"
+
+sed -i.bak "s/\${POSTGRES_USER}/$POSTGRES_USER/g" $FILE
+sed -i.bak "s/\${POSTGRES_PASSWORD}/$POSTGRES_PASSWORD/g" $FILE
+sed -i.bak "s/\${POSTGRES_HOST}/$POSTGRES_HOST/g" $FILE
+
+# Remove the backup file created by sed
+rm ${FILE}.bak
+
+cd f1-elo
+
+docker build -t f1-elo:latest .
+docker run -d --restart=always --replace -p 8000:8000 --name f1-elo f1-elo:latest
+
+cd ../
