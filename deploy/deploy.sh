f592097ac6f13e348f3c4f13e635ed8f41d04773
baaarbz <eduardob907@gmail.com>
2024-11-20 11:40:34 +0100

    feat(cd/ci): define docker file to pass env variables and use it in deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,7 +3,7 @@
 # Check if Prometheus and Postgres services are up
 if ! docker ps | grep -q prometheus || ! docker ps | grep -q postgres; then
   chmod +x ./f1-elo/deploy/deploy_infrastructure.sh
-  ./f1-elo/deploy/deploy_infrastructure.sh
+  sh f1-elo/deploy/deploy_infrastructure.sh
 fi
 
 docker pull docker.io/baaarbz/f1-elo:latest
