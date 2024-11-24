533d83a625ec41afcac262e29dde1c3e7d13b73f
baaarbz <eduardob907@gmail.com>
2024-11-24 09:40:32 +0100

    fix(ci/cd): deploy script not pulling image updated

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,9 +6,10 @@ if ! docker ps | grep -q prometheus || ! docker ps | grep -q postgres; then
   sh f1-elo/deploy/deploy_infrastructure.sh
 fi
 
+docker pull docker.io/baaarbz/f1-elo:latest
 docker run -d --restart=always --replace -p 8000:8000 --name f1-elo \
   -e POSTGRES_USER=$POSTGRES_USER \
   -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
   -e POSTGRES_URL=$POSTGRES_URL \
-  docker.io/baaarbz/f1-elo:latest
+  baaarbz/f1-elo:latest
 docker images --filter "dangling=true" -q | xargs -r docker rmi
