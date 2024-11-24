edf5d8f665c8e7627cd1d17e5f447dec1a94a984
baaarbz <eduardob907@gmail.com>
2024-11-24 12:07:57 +0100

    docs(driver-controller): update driver controller docs, to add info about the query params

--- src/main/kotlin/com/barbzdev/f1elo/infrastructure/spring/controller/DriverControllerDocumentation.kt
+++ src/main/kotlin/com/barbzdev/f1elo/infrastructure/spring/controller/DriverControllerDocumentation.kt
@@ -1,6 +1,7 @@
 package com.barbzdev.f1elo.infrastructure.spring.controller
 
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
@@ -12,32 +13,55 @@ interface DriverControllerDocumentation {
   @Operation(summary = "Get driver ELO details", description = "Fetches the ELO details of a driver by their ID")
   @ApiResponses(
     value =
-      [
-        ApiResponse(
-          responseCode = "200",
-          description = "Successfully retrieved driver ELO details",
-          content =
-            [Content(mediaType = "application/json", schema = Schema(implementation = HttpGetDriverResponse::class))]),
-        ApiResponse(responseCode = "404", description = "Driver not found", content = [Content()]),
-        ApiResponse(responseCode = "500", description = "Internal server error", content = [Content()])])
+    [
+      ApiResponse(
+        responseCode = "200",
+        description = "Successfully retrieved driver ELO details",
+        content =
+        [Content(mediaType = "application/json", schema = Schema(implementation = HttpGetDriverResponse::class))]
+      ),
+      ApiResponse(responseCode = "404", description = "Driver not found", content = [Content()]),
+      ApiResponse(responseCode = "500", description = "Internal server error", content = [Content()])]
+  )
   fun getDriver(driverId: String): ResponseEntity<HttpGetDriverResponse>
 
-  @Operation(summary = "Get drivers peak ELO listing", description = "Fetches all the drivers peak ELO with pagination")
+  @Operation(
+    summary = "Get drivers peak ELO listing",
+    description = "Fetches all the drivers peak ELO with pagination",
+    parameters = [Parameter(
+      name = "page",
+      description = "Page number from 0 to (nPages - 1)",
+      required = false,
+      example= "0"
+    ), Parameter(
+      name = "pageSize",
+      description = "Number of items per page, supported values: [25 | 50 | 100]",
+      required = false,
+    ), Parameter(
+      name = "sortBy",
+      description = "Sort by field, supported values: [currentElo | highestElo | lowestElo | id]",
+      required = false
+    ), Parameter(name = "sortOrder", description = "Sort order, supported values: [asc | desc]", required = false)]
+  )
   @ApiResponses(
     value =
-      [
-        ApiResponse(
-          responseCode = "200",
-          description = "Successfully retrieved drivers listing",
-          content =
-            [
-              Content(
-                mediaType = "application/json",
-                schema = Schema(implementation = HttpGetDriverListingResponse::class))]),
-        ApiResponse(
-          responseCode = "400",
-          description = "Required query params not present or not supported values",
-          content = [Content()]),
-        ApiResponse(responseCode = "500", description = "Internal server error", content = [Content()])])
-  fun getDriversListing(page: Int, pageSize: Int): ResponseEntity<HttpGetDriverListingResponse>
+    [
+      ApiResponse(
+        responseCode = "200",
+        description = "Successfully retrieved drivers listing",
+        content =
+        [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = HttpGetDriverListingResponse::class)
+          )]
+      ),
+      ApiResponse(
+        responseCode = "400",
+        description = "Required query params not present or not supported values",
+        content = [Content()]
+      ),
+      ApiResponse(responseCode = "500", description = "Internal server error", content = [Content()])]
+  )
+  fun getDriversListing(page: Int, pageSize: Int, sortBy: String, sortOrder: String): ResponseEntity<HttpGetDriverListingResponse>
 }
