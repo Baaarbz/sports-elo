95cdebe45bb79a48c3af0db31d7e6c27700e9ee3
baaarbz <eduardob907@gmail.com>
2024-12-01 10:59:31 +0100

    feat: allow all origins cors configuration
    (cherry picked from commit 09b24312398e9c4e3874984cb3d50ccddbbdfa9a)

--- src/main/kotlin/com/barbzdev/f1elo/infrastructure/spring/configuration/WebConfiguration.kt
+++ src/main/kotlin/com/barbzdev/f1elo/infrastructure/spring/configuration/WebConfiguration.kt
@@ -1,19 +1,23 @@
 package com.barbzdev.f1elo.infrastructure.spring.configuration
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-@EnableWebMvc
 class WebConfiguration : WebMvcConfigurer {
 
-  override fun addCorsMappings(registry: CorsRegistry) {
-    registry
-      .addMapping("/**")
-      .allowedOrigins("*")
-      .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-      .allowedHeaders("*")
+  @Bean
+  fun corsConfigurer(): WebMvcConfigurer {
+    return object : WebMvcConfigurer {
+      override fun addCorsMappings(registry: CorsRegistry) {
+        registry
+          .addMapping("/**")
+          .allowedOrigins("*")
+          .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+          .allowedHeaders("*")
+      }
+    }
   }
 }
