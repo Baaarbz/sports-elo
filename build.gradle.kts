ed197f60d07ab9c98ddea151b2a49f10fff0ae0a
baaarbz <eduardob907@gmail.com>
2024-12-08 09:49:56 +0100

    feat: add basic security for endpoints related with theoretical performance and other endpoints, excluding swagger actuator prometheus/health and drivers

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
+  implementation("org.springframework.boot:spring-boot-starter-security")
 
   implementation("io.micrometer:micrometer-registry-prometheus:1.14.1")
 
