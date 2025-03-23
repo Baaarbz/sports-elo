b795d14e2c5a6457e9d3cbd28b17f25c1f974e58
barbz <eduardob907@gmail.com>
2025-03-23 13:12:49 +0100

    tech: updated irastrucutre deploy settings

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,16 +2,16 @@
 
 # Check if Prometheus and Postgres services are up
 if ! docker ps | grep -q prometheus || ! docker ps | grep -q postgres; then
-  chmod +x ./f1-elo/deploy/deploy_infrastructure.sh
-  sh f1-elo/deploy/deploy_infrastructure.sh
+  chmod +x ./sports-elo/deploy/deploy_infrastructure.sh
+  sh sports-elo/deploy/deploy_infrastructure.sh
 fi
 
-docker pull docker.io/baaarbz/f1-elo:latest
-docker run -d --restart=always --replace -p 8000:8000 --name f1-elo \
+docker pull docker.io/baaarbz/sports-elo:latest
+docker run -d --restart=always --replace -p 8000:8000 --name sports-elo \
   -e POSTGRES_USER=$POSTGRES_USER \
   -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
   -e POSTGRES_URL=$POSTGRES_URL \
   -e AUTH_USERNAME=$AUTH_USERNAME \
   -e AUTH_PASSWORD=$AUTH_PASSWORD \
-  baaarbz/f1-elo:latest
+  baaarbz/sports-elo:latest
 docker images --filter "dangling=true" -q | xargs -r docker rmi
